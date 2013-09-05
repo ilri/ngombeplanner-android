4eec428026818d8d00e1860078d7901717a79fce
Jason Rogena <jasonrogena@gmail.com>
2013-09-05 15:29:09 +0300

    EventsActivity now supports english and swahili locales

--- MistroFarmer/src/main/java/org/cgiar/ilri/mistro/farmer/EventsActivity.java
+++ MistroFarmer/src/main/java/org/cgiar/ilri/mistro/farmer/EventsActivity.java
@@ -9,10 +9,13 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+import org.cgiar.ilri.mistro.farmer.backend.Locale;
 
 public class EventsActivity extends SherlockActivity implements View.OnClickListener
 {
-    private String localeCode;
 
     private Button addEventB;
     private Button eventHistoryB;
@@ -22,24 +25,40 @@ public class EventsActivity extends SherlockActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
-        localeCode="en";
-
         addEventB=(Button)findViewById(R.id.add_event_b);
         addEventB.setOnClickListener(this);
         eventHistoryB=(Button)findViewById(R.id.event_history_b);
         eventHistoryB.setOnClickListener(this);
 
-        initTextInViews(localeCode);
+        initTextInViews();
     }
 
-    private void initTextInViews(String localeCode)
-    {
-        if(localeCode.equals("en"))
-        {
-            setTitle(R.string.events_en);
-            addEventB.setText(R.string.add_an_event_en);
-            eventHistoryB.setText(R.string.past_events_en);
+    public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.events, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        if(item.getItemId() == R.id.action_english) {
+            Locale.switchLocale(Locale.LOCALE_ENGLISH, this);
+            initTextInViews();
+            return true;
         }
+        else if(item.getItemId() == R.id.action_swahili) {
+            Locale.switchLocale(Locale.LOCALE_SWAHILI, this);
+            initTextInViews();
+            Toast.makeText(this, "kazi katika maendeleo", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    private void initTextInViews()
+    {
+        setTitle(Locale.getStringInLocale("events",this));
+        addEventB.setText(Locale.getStringInLocale("add_an_event",this));
+        eventHistoryB.setText(Locale.getStringInLocale("past_events",this));
     }
 
     @Override
