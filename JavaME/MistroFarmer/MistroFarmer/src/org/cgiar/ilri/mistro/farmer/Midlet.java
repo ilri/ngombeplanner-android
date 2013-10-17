3daf4e872880f1d49694aedf69b429753c7ea61e
Jason Rogena <jasonrogena@gmail.com>
2013-10-17 16:29:32 +0300

    added JavaME project

---
+++ JavaME/MistroFarmer/MistroFarmer/src/org/cgiar/ilri/mistro/farmer/Midlet.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cgiar.ilri.mistro.farmer;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author jason
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
